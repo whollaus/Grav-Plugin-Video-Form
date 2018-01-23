0c773d40c5b68492af0141bf3268a62076edfed5
whollaus <wh@rotholl.at>
2018-01-23 22:26:56 +0100

    Add files via upload

---
+++ video-form/README.md
@@ -0,0 +1,33 @@
+##### Table of Contents:
+
+* [About](#about)
+* [Features](#features)
+* [Installation and Updates](#installation-and-updates)
+* [Configuration](#Configuration)
+* [Usage](#usage)
+* [TO-DO](#to-do)
+* [Contributing and Feedback](#contributing-and-feedback)
+* [License](#license)
+
+
+## About
+
+`Video-Form` is an simple plugin for [GetGrav.org](http://getgrav.org) used to add a Youtube video into your site.
+This plugin is my first Grav plugin. I use it to train myself in Grav. Use at own risk!
+
+## Features
+
+## Installation and Updates
+
+## Configuration
+
+## Usage
+
+## TO-DO
+
+- [ ] Add Vimeo
+
+## Contributing and Feedback
+
+## License
+
---
+++ video-form/video-form.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * Video-Form v0.0.1
+ *
+ * This plugin adds an Youtube video form field to the content tab.
+ *
+ * Licensed under the MIT license, see LICENSE.
+ *
+ * @package     Video-Form
+ * @version     0.0.1
+ * @link        <https://github.com/whollaus/Grav-Plugin-Video-Form>
+ * @author      Wolfgang Hollaus <wh@hollaus-it.at>
+ * @copyright   2018, Wolfgang Hollaus
+ * @license     <http://opensource.org/licenses/MIT> MIT
+ */
+
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use Grav\Common\Data\Blueprints;
+use RocketTheme\Toolbox\Event\Event;
+
+
+/**
+ *  Video Form Plugin
+ *
+ * This plugin adds an Youtube video form field to the content tab.
+ */
+class VideoFormPlugin extends Plugin
+{
+
+    /**
+     * Initialize configuration
+     */
+    public function onPluginsInitialized()
+    {
+
+        if ($this->isAdmin()) {
+            $this->active = false;
+            $events = [
+                'onBlueprintCreated' => ['onBlueprintCreated', 0]
+            ];
+            $this->enable($events);
+        }
+
+    }
+
+    /**
+     * Extend Content blueprints with the Video Form
+     *
+     * @param Event $event
+     */
+    public function onBlueprintCreated(Event $event)
+    {
+        $newtype = $event['type'];
+
+        $in_templates = $this->config->get('plugins.video-form.templates');
+
+        if (in_array($newtype, $in_templates)) {
+
+            $blueprint = $event['blueprint'];
+            if ($blueprint->get('form/fields/tabs', null, '/')) {
+
+                if (!empty($this->config->get('plugins.video-form.yt_enabled'))) {
+                    $blueprints = new Blueprints(__DIR__ . '/blueprints/');
+                    $extends = $blueprints->get('youtube');
+                    $blueprint->extend($extends, true);
+                }
+
+            }
+        }
+
+    }
+
+    /**
+     * Add Frontend Output
+     */
+    public function onPageInitialized()
+    {
+
+        $page = $this->grav['page'];
+
+        $in_templates = $this->config->get('plugins.video-form.templates');
+
+        $newtype = $page->template();
+
+        if (!empty($this->config->get('plugins.video-form.yt_enabled')) && !empty($in_templates) && !empty($newtype) && in_array($newtype, $in_templates) && isset($page->header()->yt_video)) {
+            $this->youtube($page);
+        }
+
+    }
+
+    /**
+     * Youtube Player
+     */
+    private function youtube($page)
+    {
+
+        $video_arr = parse_url($page->header()->yt_video);
+
+        $video = '';
+        if (!empty($video_arr['host']) && $video_arr['host'] == 'youtu.be' && !empty($video_arr['path'])) {
+            $video = str_replace('/', '', $video_arr['path']);
+        } elseif (!empty($video_arr['query'])) {
+            parse_str($video_arr['query'], $query);
+            if (!empty($query['v'])) {
+                $video = $query['v'];
+            }
+        }
+
+        if (!empty($video)) {
+
+            $parameters = array();
+
+            if (empty($this->config->get('plugins.video-form.yt_autoplay'))) {
+                $parameters['autoplay'] = 0;
+            } else {
+                $parameters['autoplay'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_cc_load_policy'))) {
+                $parameters['cc_load_policy'] = 0;
+            } else {
+                $parameters['cc_load_policy'] = 1;
+            }
+
+            if (!empty($this->config->get('plugins.video-form.yt_color')) && $this->config->get('plugins.video-form.yt_color') == 'red') {
+                $parameters['color'] = 'red';
+            } elseif (!empty($this->config->get('plugins.video-form.yt_color')) && $this->config->get('plugins.video-form.yt_color') == 'white') {
+                $parameters['color'] = 'white';
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_controls'))) {
+                $parameters['controls'] = 0;
+            } else {
+                $parameters['controls'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_disablekb'))) {
+                $parameters['disablekb'] = 0;
+            } else {
+                $parameters['disablekb'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_enablejsapi'))) {
+                $parameters['enablejsapi'] = 0;
+            } else {
+                $parameters['enablejsapi'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_fs'))) {
+                $parameters['fs'] = 0;
+            } else {
+                $parameters['fs'] = 1;
+            }
+
+            if (!empty($this->config->get('plugins.video-form.yt_hl'))) {
+                $parameters['hl'] = $this->config->get('plugins.video-form.yt_hl');
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_iv_load_policy'))) {
+                $parameters['iv_load_policy'] = 0;
+            } else {
+                $parameters['iv_load_policy'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_loop'))) {
+                $parameters['loop'] = 0;
+            } else {
+                $parameters['loop'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_modestbranding'))) {
+                $parameters['modestbranding'] = 0;
+            } else {
+                $parameters['modestbranding'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_origin'))) {
+                $parameters['origin'] = '';
+            } else {
+                if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+                    $parameters['origin'] = 'https://' . $_SERVER['REMOTE_ADDR'];
+                } else {
+                    $parameters['origin'] = 'http://' . $_SERVER['REMOTE_ADDR'];
+                }
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_rel'))) {
+                $parameters['rel'] = 0;
+            } else {
+                $parameters['rel'] = 1;
+            }
+
+            if (empty($this->config->get('plugins.video-form.yt_showinfo'))) {
+                $parameters['showinfo'] = 0;
+            } else {
+                $parameters['showinfo'] = 1;
+            }
+
+            $video_content = '<div class="video-form">';
+            $video_content .= '<div class="video-form-yt">';
+            $video_content .= '<iframe src="https://www.youtube.com/embed/' . $video . '?' . http_build_query($parameters) . "\n" . '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+            $video_content .= '</div>';
+            $video_content .= '</div>';
+
+            $page->setRawContent($page->content() . $video_content);
+
+            $this->grav['assets']->add('plugin://video-form/css/video-form.css');
+
+        }
+
+    }
+
+}
